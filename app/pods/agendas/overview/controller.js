d04d0569831fb963b749e586adfc385ffff57722
Flor Sanders <me@florsanders.be>
2021-06-23 12:07:31 +0200

    [refactor] octanified agendas overview controller (incomplete)

--- app/pods/agendas/overview/controller.js
+++ app/pods/agendas/overview/controller.js
@@ -1,89 +1,95 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import moment from 'moment';
-import DefaultQueryParamsMixin from 'ember-data-table/mixins/default-query-params';
-import { inject } from '@ember/service';
-import { computed } from '@ember/object';
 
-export default Controller.extend(DefaultQueryParamsMixin, {
-  sessionService: inject(),
-  intl: inject(),
-  agendaService: inject(),
+export default class AgendasOverviewController extends Controller {
+  @service sessionService;
+  @service intl;
+  @service agendaService;
 
-  queryParams: ['from', 'to'],
-  dateFilter: '',
+  queryParams = ['from', 'to'];
 
-  dateRegex: /^(?:(\d{1,2})-)?(?:(\d{1,2})-)?(\d{4})$/,
+  @tracked dateFilter = '';
+  dateRegex = /^(?:(\d{1,2})-)?(?:(\d{1,2})-)?(\d{4})$/;
+  sort = '-planned-start,number-representation';
+  page = 0;
+  size = 10;
+  filter = '';
 
-  sort: '-planned-start,number-representation',
-  size: 10,
+  get activeAgendas() {
+    console.warn('Not implemented: Getting active agendas');
+    return null;
+  }
 
-  activeAgendas: computed('model', async function() {
-    const dateOfToday = moment().seconds(0)
-      .milliseconds(0)
-      .minutes(0)
-      .hours(0)
-      .utc()
-      .subtract(1, 'weeks')
-      .format();
-    const meetings = await this.store.query('meeting', {
-      filter: {
-        ':gte:planned-start': dateOfToday,
-      },
-      sort: 'planned-start,number-representation',
-    });
-    const activeAgendas = await this.agendaService.getActiveAgendas(dateOfToday);
+  // activeAgendas: computed('model', async function() {
+  //   const dateOfToday = moment().seconds(0)
+  //     .milliseconds(0)
+  //     .minutes(0)
+  //     .hours(0)
+  //     .utc()
+  //     .subtract(1, 'weeks')
+  //     .format();
+  //   const meetings = await this.store.query('meeting', {
+  //     filter: {
+  //       ':gte:planned-start': dateOfToday,
+  //     },
+  //     sort: 'planned-start,number-representation',
+  //   });
+  //   const activeAgendas = await this.agendaService.getActiveAgendas(dateOfToday);
 
-    return meetings.filter((meeting) => this.checkIfAgendaIsPresent(activeAgendas, meeting));
-  }),
+  //   return meetings.filter((meeting) => this.checkIfAgendaIsPresent(activeAgendas, meeting));
+  // }),
 
-  checkIfAgendaIsPresent(activeAgendas, meeting) {
-    const foundItem = activeAgendas.find((activeAgenda) => activeAgenda.meeting_id === meeting.id);
+  // checkIfAgendaIsPresent(activeAgendas, meeting) {
+  //   const foundItem = activeAgendas.find((activeAgenda) => activeAgenda.meeting_id === meeting.id);
+  //   if (!foundItem) {
+  //     return false;
+  //   }
+  //   return true;
+  // }
 
-    if (!foundItem) {
-      return false;
+  @action
+  setDateFilter(date) {
+    const newDate = date.split('/').join('-');
+    const match = this.dateRegex.exec(newDate);
+    if (!match) {
+      this.set('from', undefined);
+      this.set('to', undefined);
+      return;
     }
-    return true;
-  },
+    const min = moment(parseInt(match[3], 10), 'YYYY', true);
+    let unitToAdd;
+    if (match[1] && match[2]) {
+      unitToAdd = 'day';
+      min.set('date', parseInt(match[1], 10));
+      min.set('month', parseInt(match[2], 10) - 1); // Count starts from 0
+    } else if (match[1]) {
+      unitToAdd = 'month';
+      min.set('month', parseInt(match[1], 10) - 1);
+    } else {
+      unitToAdd = 'year';
+    }
+    const max = min.clone().add(1, `${unitToAdd}s`);
 
-  actions: {
-    setDateFilter(date) {
-      const newDate = date.split('/').join('-');
-      const match = this.dateRegex.exec(newDate);
-      if (!match) {
-        this.set('from', undefined);
-        this.set('to', undefined);
-        return;
-      }
-      const min = moment(parseInt(match[3], 10), 'YYYY', true);
-      let unitToAdd;
-      if (match[1] && match[2]) {
-        unitToAdd = 'day';
-        min.set('date', parseInt(match[1], 10));
-        min.set('month', parseInt(match[2], 10) - 1); // Count starts from 0
-      } else if (match[1]) {
-        unitToAdd = 'month';
-        min.set('month', parseInt(match[1], 10) - 1);
-      } else {
-        unitToAdd = 'year';
-      }
-      const max = min.clone().add(1, `${unitToAdd}s`);
+    this.set('from', min.format('YYYY-MM-DD'));
+    this.set('to', max.format('YYYY-MM-DD'));
+    this.set('page', 0);
+  }
 
-      this.set('from', min.format('YYYY-MM-DD'));
-      this.set('to', max.format('YYYY-MM-DD'));
-      this.set('page', 0);
-    },
+  @action
+  clearFilter() {
+    this.set('to', null);
+    this.set('from', null);
+    this.set('dateFilter', '');
+  }
 
-    clearFilter() {
-      this.set('to', null);
-      this.set('from', null);
-      this.set('dateFilter', '');
-    },
-
-    onClickRow(meeting) {
-      meeting.get('latestAgenda').then((latestAgenda) => {
-        const latestAgendaId = latestAgenda.get('id');
-        this.transitionToRoute('agenda.agendaitems', meeting.id, latestAgendaId);
-      });
-    },
-  },
-});
+  @action
+  onClickRow(meeting) {
+    meeting.get('latestAgenda').then((latestAgenda) => {
+      const latestAgendaId = latestAgenda.get('id');
+      this.transitionToRoute('agenda.agendaitems', meeting.id, latestAgendaId);
+    });
+  }
+}
