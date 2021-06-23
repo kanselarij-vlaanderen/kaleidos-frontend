2334c1b67d493c767758a9726a45149267240a59
Flor Sanders <me@florsanders.be>
2021-06-23 11:55:18 +0200

    [refactor] Octanified agendas overview route + removed DataTableRouteMixin

--- app/pods/agendas/overview/route.js
+++ app/pods/agendas/overview/route.js
@@ -1,23 +1,31 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 import moment from 'moment';
-import { inject } from '@ember/service';
-import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
-export default Route.extend(DataTableRouteMixin, {
-  agendaService: inject(),
-  modelName: 'meeting',
+export default class AgendaOverviewRoute extends Route {
+  @service agendaService;
 
-  queryParams: {
+  queryParams= {
     from: {
       refreshModel: true,
     },
     to: {
       refreshModel: true,
     },
-  },
+  }
 
-  mergeQueryOptions(params) {
-    const options = {};
+  async model(params) {
+    const options = {
+      sort: params.sort,
+      page: {
+        number: params.page,
+        size: params.size,
+      },
+    };
+    if (params.filter) {
+      options.filter = params.filter;
+    }
     if (params.from) {
       options['filter[:gte:planned-start]'] = moment(params.from, 'YYYY-MM-DD').utc()
         .format();
@@ -26,14 +34,11 @@ export default Route.extend(DataTableRouteMixin, {
       options['filter[:lte:planned-start]'] = moment(params.to, 'YYYY-MM-DD').utc()
         .format();
     }
-    return options;
-  },
+    return this.store.query('meeting', options);
+  }
 
-  actions: {
-    refreshRoute() {
-      this._super(...arguments);
-      this.refresh();
-    },
-  },
-
-});
+  @action
+  refreshModel() {
+    this.refresh();
+  }
+}
